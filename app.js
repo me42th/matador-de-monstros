70c18ea452bab764e6110372f2344e21914763db
me42th <david.meth@hotmail.com>
2020-02-15 18:49:12 -0300

    customizacao score

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 new Vue({
     el: '#app',
     data: {
-
+        playerLife: 100,
+        monsterLife: 100
     },
     computed: {
-
+        playerColor(){
+            return this.playerLife > 20?'normal':'danger';
+        },
+        monsterColor(){
+            return this.monsterLife > 20?'normal':'danger';
+        }
     },
     methods: {
 
     },
     watch: {
-        
+
     }
 })
