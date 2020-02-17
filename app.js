ee79b3b5bc30c02a9c6e3232469f33ddf2e35130
David Meth <david.meth@avansys.com.br>
2020-02-17 09:36:22 -0300

    matador_de_tickets init

--- app.js
+++ app.js
@@ -2,30 +2,35 @@ new Vue({
     el: '#app',
     data: {
         playerLife: 100,
-        monsterLife: 100, 
+        ticketLife: 100, 
         running: false,
-        logs: []
+        logs: [],
+        acoes: [ 'O Cliente Acordou de Mau Humor e Você Se Fuuu','Não Teve Café Hoje e Você Se Fuuu'],
+        ticketNumber: '???',
+        relaxar: ['Tomar Uma Cerveja','Ir na Praia','Fazer Sexo','Comer Besteira','Tocar Guitarra','Ir na Casa de Massagem'],
+        rlxAtual: 'Relaxar Um Pouco'
     },
     computed: {
         playerColor(){
             return this.playerLife > 20?'normal':'danger';
         },
-        monsterColor(){
-            return this.monsterLife > 20?'normal':'danger';
+        ticketColor(){
+            return this.ticketLife > 20?'normal':'danger';
         },
         hasResult(){
-            return this.playerLife == 0 || this.monsterLife == 0;
+            return this.playerLife == 0 || this.ticketLife == 0;
         }
     },
     methods: {
         startGame(){
             this.running = true;
             this.playerLife = 100;
-            this.monsterLife = 100;
+            this.ticketLife = 100;
             this.logs = [];
+            this.ticketNumber = Math.round(Math.random() * 2000);
         },
         attack(especial){
-            this.hurt('monsterLife',5,10,especial);
+            this.hurt('ticketLife',5,10,especial);
             if(!this.hasResult){
                 setTimeout(
                     () => this.hurt('playerLife',10,13,false)
@@ -37,9 +42,9 @@ new Vue({
             const plus = especial?5:0;
             const hurt = this.getRandom(min+plus, max+plus);
             if(prop == 'playerLife'){
-                this.registerLog(`Destruidor Atacou e Tirou ${hurt} de Murilo`,'monster');
+                this.registerLog(`O Ticket não Foi Homologado, o Dev Perde ${hurt} de Vida`,'ticket');
             } else {
-                this.registerLog(`Murilo Atacou e Tirou ${hurt} do Destruidor`,'player');
+                this.registerLog(`O Dev Codificou e resolveu ${hurt}% do Ticket`,'player');
             }
             this[prop] = Math.max(this[prop] - hurt, 0);
         },
@@ -48,6 +53,7 @@ new Vue({
             return Math.round(value);
         },
         healAndHurt(){
+            this.rlxAtual = this.relaxar[(Math.round(Math.random() * (this.relaxar.lenght -1 )))];
             this.heal(20,25);
             setTimeout(
                 () => this.hurt('playerLife',10,13,false)
@@ -55,7 +61,7 @@ new Vue({
         },
         heal(min,max){
             const heal = this.getRandom(min,max);
-            this.registerLog(`Murilo Curou ${heal}`,'player');
+            this.registerLog(`O Dev Relaxou um Pouco e Curou ${heal}% de Vida`,'player');
             this.playerLife = Math.min(this.playerLife + heal,100);
         },
         registerLog(txt, cls){
